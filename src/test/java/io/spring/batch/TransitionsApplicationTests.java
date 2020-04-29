3761b4203067b0a6b596e04edcadda6d7aaab7c5
Wagner Martins <wagnermartins@wagnermac.local>
2020-04-29 16:32:17 -0300

    first commit

---
+++ src/test/java/io/spring/batch/TransitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
